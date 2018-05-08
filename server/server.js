a29c1fa7790938a91269f580e00a1d5448fdc9c4
ADHARMAN\ininpkum <prashant.kumar2@harman.com>
2018-05-08 23:06:57 +0530

    added connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
+'use strict';
 const express = require('express');
 const path = require('path');
-
-
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log("new user connected");
+});
+
+io.on('disconnect', (socket) => {
+    console.log('user disconnected');
+});
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`node server started at port ${port}`);
 });
